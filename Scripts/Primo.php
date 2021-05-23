5533ee13cb11a84bccb40bc29bd1306620c454de
Roberto_Cornacchiari <56387052+RobertoCornacchiari@users.noreply.github.com>
2021-05-23 14:54:19 +0200

    Configurazione e connessione con il database

--- Scripts/Primo.php
+++ Scripts/Primo.php
@@ -3,5 +3,9 @@
 include_once "config.php";
 
 //Ricevo i dati dal body della richiesta in formato JSON
+/*
 $rawdata = file_get_contents("php://input");
 $rawdata = json_decode($rawdata, true);
+*/
+
+echo "connessione";
