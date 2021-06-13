a044e566361c08fb441fa6c63f0060cb33625f50
Roberto_Cornacchiari <56387052+RobertoCornacchiari@users.noreply.github.com>
2021-06-13 11:38:39 +0200

    Correzione errore

--- Scripts/ProiezioniDisponibili.php
+++ Scripts/ProiezioniDisponibili.php
@@ -2,7 +2,7 @@
 
 include_once "config.php";
 
-$stmt = $pdo->query("SELECT proiezione.id, proiezione.dataOra, proiezione.idSala, film.titolo FROM proiezione, film WHERE proiezione.idFilm = film.id AND DATE_ADD(now(), INTERVAL -10 minute ) < proiezione.dataOra");
+$stmt = $pdo->query("SELECT proiezione.id, proiezione.dataOra, proiezione.idSala, film.titolo FROM proiezione, film WHERE proiezione.idFilm = film.id AND DATE_ADD(now(), INTERVAL -10 minute ) < proiezione.dataOra AND DAY(now()) = DAY(proiezione.dataOra)");
 
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
